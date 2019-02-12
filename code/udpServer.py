d69c0d43ed86cad7636e0a3907fda4eb1466b970
13197963 <46889718+13197963@users.noreply.github.com>
2019-02-12 14:26:12 +1100

    Update udpServer.py
    Fix tabbing for windows, altered server address to make windows firewall exemption

--- code/udpServer.py
+++ code/udpServer.py
@@ -7,7 +7,7 @@ contactList =[]
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 10000)
+server_address = ("192.168.0.7", 10000)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
@@ -15,13 +15,13 @@ while True:
     print >>sys.stderr, '\nwaiting to receive message'
     data, address = sock.recvfrom(4096)
     if address not in contactList:
-    	contactList.append(address)
+        contactList.append(address)
 
     
     print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
     print >>sys.stderr, data
     
     if data:
-    	for address in contactList:
-        	sent = sock.sendto(data, address)
-        	print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
+        for address in contactList:
+            sent = sock.sendto(data, address)
+            print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
